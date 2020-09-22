a86fe48b2f71f1896a6d04144fbd694d069311be
arhamarshad1 <68512793+arhamarshad1@users.noreply.github.com>
2020-09-22 10:01:45 -0300

    Update KALPHA.R

--- KALPHA.R
+++ KALPHA.R
@@ -5,26 +5,20 @@ commIssues2 <- Test$commIssues_J2
 commIssues3 <- Test$commIssues_J3
 commIssues4 <- Test$commIssues_J4
 
-library(lpSolve)
-library(irr)
-kripp.alpha(rbind(commIssues1,commIssues2,commIssues3,commIssues4),"ratio")
+> library(lpSolve)
+> library(irr)
+> kripp.alpha(rbind(commIssues1,commIssues2,commIssues3,commIssues4),"ratio")
+ Krippendorff's alpha
 
-trustIssues1 <- Test$trustIssues_J1
-trustIssues2 <- Test$trustIssues_J2
-trustIssues3 <- Test$trustIssues_J3
-trustIssues4 <- Test$trustIssues_J4
-
-kripp.alpha(rbind(trustIssues1,trustIssues2,trustIssues3,trustIssues4),"ratio")
-
-totStaff1 <- Test$totalStaff_J1
-totStaff2 <- Test$totalStaff_J2
-totStaff3 <- Test$totalStaff_J3
-totStaff4 <- Test$totalStaff_J4
-
-kripp.alpha(rbind(totStaff1,totStaff2,totStaff3,totStaff4),"ratio")
-
-##OUTPUT##
-
+ Subjects = 30 
+   Raters = 4 
+    alpha = 0.667 
+> 
+> trustIssues1 <- Test$trustIssues_J1
+> trustIssues2 <- Test$trustIssues_J2
+> trustIssues3 <- Test$trustIssues_J3
+> trustIssues4 <- Test$trustIssues_J4
+> 
 > kripp.alpha(rbind(trustIssues1,trustIssues2,trustIssues3,trustIssues4),"ratio")
  Krippendorff's alpha
 
@@ -37,12 +31,6 @@ kripp.alpha(rbind(totStaff1,totStaff2,totStaff3,totStaff4),"ratio")
 > totStaff3 <- Test$totalStaff_J3
 > totStaff4 <- Test$totalStaff_J4
 > 
-> kripp.alpha(rbind(totStaff1,totStaff2,totStaff3,totStaff4),"ratio")
- Krippendorff's alpha
-
- Subjects = 30 
-   Raters = 4 
-    alpha = 0.727 
 > kripp.alpha(rbind(totStaff1,totStaff2,totStaff3,totStaff4),"ratio")
  Krippendorff's alpha
 
